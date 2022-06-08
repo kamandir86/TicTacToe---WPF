faae6d817e8e782b37a64c2053c3493fcbf6d304
Bartek <Bartek@MOLDOW-PC78>
2022-06-08 20:46:10 +0200

    Add project files.

---
+++ BoardLogic/Board.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoardLogic
+{
+    public class Board
+    {
+        
+        public int[] Grid { get; set; }
+        public static string CurrentPlayer { get; set; } = X;
+        private const string X = "X";
+        private const string O = "O";
+
+        // SPRAWDZIC CZY KONIECZNE
+
+        //public Board()
+        //{
+        //    for (int i = 0; i < 9; i++)
+        //    {
+        //       // Grid[i] = 0;
+        //    }
+
+        //}
+
+
+        public static void SetNextPlayer()
+        {
+            if (CurrentPlayer == X)
+                CurrentPlayer = O;
+            else
+                CurrentPlayer = X;
+        }
+
+        public static bool CheckForWinner()
+        {
+            // check first row
+            for (int i = 0; i < 3; i++)
+            {
+                // check if not blank
+                if (true)
+                {
+
+                }
+                // if 1==2 && 1==3 return true using Button tags
+            }
+
+            // the same for columns
+
+            // the same for diagonal
+
+            return false;
+        }
+    }
+
+    
+}
---
+++ TicTacToe - WPF/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using BoardLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TicTacToe___WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Board game = new Board();
+
+        }
+        
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var space = (Button)sender;
+            if (!String.IsNullOrWhiteSpace(space.Content?.ToString())) return;
+            space.Content = Board.CurrentPlayer;
+            // setting X or O in place of a button
+            Board.SetNextPlayer();
+            // changing the player sign
+            
+        }
+
+        private void NewGameButton(object sender, RoutedEventArgs e)
+        {
+            foreach (var item in fieldBoard.Children)
+            {
+                if (item is Button)
+                {
+                    ((Button)item).Content = String.Empty;
+                }
+            }
+        }
+    }
+}
